d866019fc102677c170e5261bba05a529863ec77
cjp <No5stranger@gmail.com>
2019-07-23 14:44:03 +0800

    not init min

--- leetcode/stack_min.go
+++ leetcode/stack_min.go
@@ -3,52 +3,58 @@ package leetcode
 import "fmt"
 
 type Stack struct {
-	data []int
-	top  int
-	min  int
+	data []int64
+	top  int64
+	min  int64
 }
 
 func NewStack() *Stack {
-	return &Stack{data: make([]int, 0), top: -1, min: 0}
+	return &Stack{data: make([]int64, 0), top: -1}
 }
 
 func (stack *Stack) IsEmpty() bool {
 	return stack.top < 0
 }
 
-func (stack *Stack) Push(d int) {
+func (stack *Stack) Push(d int64) {
 	if stack.top < 0 {
 		stack.top = 0
+		stack.min = d
+		stack.data = append(stack.data, d)
 	} else {
 		stack.top++
+		stack.data = append(stack.data, d-stack.min)
 	}
-	stack.data = append(stack.data, d-stack.min)
 	if d < stack.min {
 		stack.min = d
 	}
 }
 
-func (stack *Stack) Pop() int {
+func (stack *Stack) Pop() int64 {
 	if stack.IsEmpty() {
 		return 0
 	}
-	var vd int
+	var vd int64
 	sd := stack.data[stack.top]
-	if sd < 0 {
-		vd = stack.min
-		stack.min = stack.min - sd
+	if stack.top == 0 {
+		vd = sd
 	} else {
-		vd = sd + stack.min
+		if sd < 0 {
+			vd = stack.min
+			stack.min = stack.min - sd
+		} else {
+			vd = sd + stack.min
+		}
 	}
 	stack.top--
 	return vd
 }
 
-func (stack *Stack) Top() int {
+func (stack *Stack) Top() int64 {
 	if stack.IsEmpty() {
 		return 0
 	}
-	var vd int
+	var vd int64
 	sd := stack.data[stack.top]
 	if sd < 0 {
 		vd = stack.min
@@ -58,7 +64,7 @@ func (stack *Stack) Top() int {
 	return vd
 }
 
-func (stack *Stack) GetMin() int {
+func (stack *Stack) GetMin() int64 {
 	return stack.min
 }
 
@@ -66,7 +72,7 @@ func (stack *Stack) Print() {
 	if stack.IsEmpty() {
 		fmt.Println("empty stack")
 	}
-	for i := 0; i <= stack.top; i++ {
+	for i := int64(0); i <= stack.top; i++ {
 		fmt.Printf("stack i:%d v:%d\n", i, stack.data[i])
 	}
 }
--- leetcode/stack_min_test.go
+++ leetcode/stack_min_test.go
@@ -3,12 +3,11 @@ package leetcode
 import "testing"
 
 func TestMinStack(t *testing.T) {
-	//stack := NewStack()
-	stack := NewMinStack()
+	stack := NewStack()
+	//stack := NewMinStack()
 	stack.Push(-2)
 	stack.Push(0)
 	stack.Push(-3)
-	stack.Push(0)
 	//stack.Print()
 	t.Log(stack.GetMin())
 	stack.Pop()
